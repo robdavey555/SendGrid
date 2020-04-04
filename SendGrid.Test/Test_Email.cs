4e5a4d51160111b9f10c24302a9d0ce4981ff389
Rob Davey <robd@weddingshop.com>
2020-04-04 18:03:15 +0100

    Add start of Integration test

---
+++ SendGrid.Test/Test_Email.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using SendGrid.Service;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SendGrid.Test
+{
+    public class Test_Email
+        : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public Test_Email(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact(DisplayName = "Email")]
+        public async Task Email_BadRequest()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            Dictionary<string, string> Substitutions = new Dictionary<string, string>();
+            Substitutions.Add("sub", "me");
+            var request = new StringContent(JsonConvert.SerializeObject(Substitutions), Encoding.UTF8, "application/json");
+            string email = "joeblogs@email.com";
+            string templateid = "897890y8h9sdy78dyf";
+
+            // Act
+            var response = await client.PostAsync($"api/email/SendEmail?emailAddress={email}&templateid={templateid}", request);
+
+            // Assert
+            Assert.True(response.StatusCode == System.Net.HttpStatusCode.BadRequest);
+        }
+    }
+}
---
+++ SendGrid.Test/Tests_Healthcheck.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using SendGrid.Service;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SendGrid.Test
+{
+    public class Tests_HealthCheck
+    : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public Tests_HealthCheck(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact(DisplayName = "HealthCheck")]
+        public async Task HealthCheck()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("health");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(response.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+    }
+}
